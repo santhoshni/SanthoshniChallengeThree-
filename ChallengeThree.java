8d7919a9b3836ecb2ef847c7627fe65933f09f26
santhoshni <70932006+santhoshni@users.noreply.github.com>
2022-03-06 09:25:47 -0600

    method update

--- ChallengeThree.java
+++ ChallengeThree.java
@@ -1,19 +1,25 @@
 public class ChallengeThree {
-    public static void main(String args[]) {
-   
+	
+    static void decodeStr(){
         Map<Character, Character> replacements = new HashMap<>();
         replacements.put('h', 'b');
         replacements.put('g', 'u');
         replacements.put('i', 't');
         replacements.put('k', 'e');
         replacements.put('f', 'r');
-
+    
         String input = "hgiikf";
         StringBuilder output = new StringBuilder();
         for (Character c : input.toCharArray()) {
             output.append(replacements.getOrDefault(c, c));
         }
         System.out.println(output.toString());
-	
+    }
+    
+    public static void main(String args[]) {
+        
+        ChallengeThree CT = new ChallengeThree();
+        CT.decodeStr();
+ 	
     }
 }
